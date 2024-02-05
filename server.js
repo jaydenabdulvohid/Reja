04b669e00fb1aefd6fb3c21d671cd737001353f4
jaydenabdulvohid <hakimovabdulvohid85@gmail.com>
2024-02-05 21:42:25 +0900

    feat: develop author router ejs  page

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) =>{
+if(err) {
+console.log("ERROR:", err);
+}else {
+  user = JSON.parse(data)
+}
+
+});
+
+
 
 // 1 = Kirish kodlari 
 app.use(express.static("public"));
@@ -37,9 +52,14 @@ res.end(`<h1 style="background: grey">ERIK HAYDAR</h1>`);
 
 app.post("/create-item", (req, res) => {
     //console.log(req.body);
-    console.log(req);
-    res.json({ test: "success" });
+    // console.log(req);
+    // res.json({ test: "success" });
 });
+
+app.get("/author", (req, res) =>{
+res.render("author",{user: user});
+});
+
 
 
 app.get("/", function (req, res){
