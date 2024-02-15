2968f61d8cbb2a047aa55487fcbd7b1c9486f1a0
jaydenabdulvohid <hakimovabdulvohid85@gmail.com>
2024-02-15 14:59:28 +0900

    feat: Reja loyihamizning yakuniy bosqichi logic

--- app.js
+++ app.js
@@ -33,9 +33,29 @@ app.post("/delete-item", (req, res) => {
   db.collection("plans").deleteOne(
     { _id: new mongodb.ObjectId(id) },
     function (err, data) {
-      res.json({ state: "succes" });
+      res.json({ state: "success" });
     }
   );
+});
+
+app.post("/edit-item", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  db.collection("plans").findOneAndUpdate(
+    { _id: new mongodb.ObjectId(data.id) },
+    { $set: { reja: data.new_input } },
+    function (err, data) {
+      res.json({ state: "success" });
+    }
+  );
+});
+
+app.post("/delete-all", (req, res) => {
+  if (req.body.delete_all) {
+    db.collection("plans").deleteMany(function () {
+      res.json({ state: "hamma rejalar ochirildi" });
+    });
+  }
 });
 
 app.get("/", function (req, res) {
